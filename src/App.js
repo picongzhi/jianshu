d50c36eb50b12f7bd9eae2dc442f99b9fda27b77
picongzhi <picongzhi@gmail.com>
2020-02-11 00:41:18 +0800

    feat(home): add home page layout

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import React, {
-  Component
-} from 'react';
+import React, {Component} from 'react';
 import store from './store';
-import {
-  Provider
-} from 'react-redux';
-
-import {
-  GlobalStyle
-} from './style';
-import {
-  IconFont
-} from "./statics/iconfont/iconfont";
-
+import {Provider} from 'react-redux';
+import {Route, BrowserRouter} from 'react-router-dom';
+import {GlobalStyle} from './style';
+import {IconFont} from "./statics/iconfont/iconfont";
 import Header from './common/header';
+import Home from './pages/home';
+import Detail from './pages/detail';
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <GlobalStyle/>
-        <IconFont/>
-        <Header/>
+        <div>
+          <GlobalStyle/>
+          <IconFont/>
+
+          <Header/>
+          <BrowserRouter>
+            <div>
+              <Route path='/' exact component={Home}/>
+              <Route path='/detail' exact component={Detail}/>
+            </div>
+          </BrowserRouter>
+        </div>
       </Provider>
     );
   }
---
+++ src/pages/detail/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Topic extends Component {
+  render() {
+    return (
+      <div>home</div>
+    )
+  }
+}
+
+export default Topic;
